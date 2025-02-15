c86b5635321ca8186b8098858448018095bca7de
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-02-15 19:48:28 +0300

    feat: :sparkles: Add file PostData shape

---
+++ boosty_downloader/src/boosty_api/models/post/post_data_types/post_data_file.py
@@ -0,0 +1,13 @@
+"""The module with file representation of posts data"""
+
+from boosty_api.models.post.base_post_data import BasePostData
+
+from boosty_downloader.src.boosty_api.models.post.post_data_type import PostDataType
+
+
+class PostDataFile(BasePostData):
+    """File content piece in posts"""
+
+    type = PostDataType.file
+    url: str
+    title: str
