c2f09cfc44ce26663335ce6493c8595a82a4f104
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-02-16 13:20:33 +0300

    fix: :bug: Fix import from root

--- boosty_downloader/src/boosty_api/models/post/post_data_types/post_data_file.py
+++ boosty_downloader/src/boosty_api/models/post/post_data_types/post_data_file.py
@@ -1,8 +1,7 @@
 """The module with file representation of posts data"""
 
 from boosty_api.models.post.base_post_data import BasePostData
-
-from boosty_downloader.src.boosty_api.models.post.post_data_type import PostDataType
+from boosty_api.models.post.post_data_type import PostDataType
 
 
 class PostDataFile(BasePostData):
