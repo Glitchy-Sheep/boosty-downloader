72fed0f73d35821e600e26ead123bd31ac65b2b6
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-02-16 15:16:24 +0300

    feat: :sparkles: Refactor models and add pagination option to get_author_posts method

--- boosty_downloader/src/boosty_api/core/client.py
+++ boosty_downloader/src/boosty_api/core/client.py
@@ -1,8 +1,13 @@
 """Boosty API client for accessing content."""
 
+from __future__ import annotations
+
 import aiohttp
 
+from boosty_downloader.src.boosty_api.models.post.extra import Extra
 from boosty_downloader.src.boosty_api.models.post.post import Post
+from boosty_downloader.src.boosty_api.models.post.posts_request import PostsResponse
+from boosty_downloader.src.boosty_api.utils.filter_none_params import filter_none_params
 
 
 class BoostyAPIClient:
@@ -15,12 +20,34 @@ class BoostyAPIClient:
     def __init__(self, session: aiohttp.ClientSession) -> None:
         self.session = session
 
-    async def get_author_posts(self, author_name: str) -> list[Post]:
+    async def get_author_posts(
+        self,
+        author_name: str,
+        offset: str | None = None,
+    ) -> PostsResponse:
+        """
+        Request to get posts from the specified author.
+
+        The request supports pagination, so the response contains meta info.
+        If you want to get all posts, you need to repeat the request with the offset of previous response
+        until the 'is_last' field becomes True.
+        """
         endpoint = f'blog/{author_name}/post/'
 
-        posts_raw = await self.session.get(endpoint)
+        posts_raw = await self.session.get(
+            endpoint,
+            params=filter_none_params(
+                {
+                    'offset': offset,
+                },
+            ),
+        )
         posts_data = await posts_raw.json()
-        posts = posts_data['data']
 
-        result: list[Post] = [Post.model_validate(post) for post in posts]
-        return result
+        posts: list[Post] = [Post.model_validate(post) for post in posts_data['data']]
+        extra: Extra = Extra.model_validate(posts_data['extra'])
+
+        return PostsResponse(
+            posts=posts,
+            extra=extra,
+        )
