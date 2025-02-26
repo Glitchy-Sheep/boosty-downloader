693b38131b6a5b28c8ea44e488e28f8a3f95918e
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-02-26 18:41:17 +0300

    refactor: :recycle: Refactor content extractor to work only with content string part

--- boosty_downloader/src/download_manager/content_extractors/textual_post_extractor.py
+++ boosty_downloader/src/download_manager/content_extractors/textual_post_extractor.py
@@ -4,39 +4,25 @@ from __future__ import annotations
 
 import json
 from io import StringIO
-from typing import TYPE_CHECKING
-
-from boosty_downloader.src.boosty_api.models.post.post_data_types.post_data_link import (
-    PostDataLink,
-)
-
-if TYPE_CHECKING:
-    from boosty_downloader.src.boosty_api.models.post.post_data_types.post_data_text import (
-        PostDataText,
-    )
 
 
-async def download_textual_content(
-    textual_content: list[PostDataText | PostDataLink],
-) -> StringIO:
-    """Extract textual content from a post by its chunks"""
-    post_text = StringIO()
+def extract_textual_content(
+    content: str,
+) -> str:
+    """Extract textual content from a post chunk Link/Text"""
+    buffer = StringIO()
 
     # Merge all the text and link fragments into one file
-    for text_chunk in textual_content:
-        try:
-            json_data: list[str] = json.loads(text_chunk.content)
-        except json.JSONDecodeError:
-            continue
+    try:
+        json_data: list[str] = json.loads(content)
+    except json.JSONDecodeError:
+        return buffer.getvalue()
 
-        if len(json_data) == 0:
-            continue
+    if len(json_data) == 0:
+        return buffer.getvalue()
 
-        clean_text = str(json_data[0])
+    clean_text = str(json_data[0])
 
-        if isinstance(text_chunk, PostDataLink):
-            clean_text += f' [{text_chunk.url}]'
+    buffer.write(clean_text + '\n')
 
-        post_text.write(clean_text + '\n')
-
-    return post_text
+    return buffer.getvalue()
